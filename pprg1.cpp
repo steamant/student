7949ffbdb20b670e8f2a8b3cc5413a8d2e415dd1
Bart Waszczyk <steamant@wp.pl>
2023-09-30 12:53:47 +0200

    Add files via upload

---
+++ pprg1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "podstawy programowania \n";
+    return 0;
+}
---
+++ pprg_z1-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Bart \n";
+    cout << "32351 \n";
+    cout << "Polsko-Japońska Wyższa Szkoła Technik Komputerowych \n";
+    return 0;
+}
+
+
